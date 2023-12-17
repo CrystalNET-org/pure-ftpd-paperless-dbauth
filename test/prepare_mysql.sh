7d4ca151ce28cbbab5a69b1ffd7810559834ede8
Lukas Wingerberg <bigluks@gmail.com>
2023-12-17 14:16:59 +0100

    add test for mysqkl

--- test/prepare_mysql.sh
+++ test/prepare_mysql.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-psql_host=$(env | grep PORT_5432_TCP_ADDR | cut -d "=" -f 2)
-echo "Waiting for postgresql to launch on ${psql_host}:5432..."
+mysql_host=$(env | grep PORT_3306_TCP_ADDR | cut -d "=" -f 2)
+echo "Waiting for mysql to launch on ${mysql_host}:3306..."
 
-timeout 22 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' "${psql_host}" 5432
+timeout 22 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' "${mysql_host}" 3306
 
-echo "postgresql launched, injecting sql"
-psql -h "${psql_host}" -d test -p 5432 -U test -f ./test/prep_sql.sql
+echo "mysql launched, injecting sql"
+mysql --host="${mysql_host}" --port=5432 --user=test --password=example --database=test < ./test/prep_sql.sql
---
+++ test/test_mysql_pw.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mysql_host=$(env | grep PORT_3306_TCP_ADDR | cut -d "=" -f 2)
+echo "Waiting for mysql to launch on ${mysql_host}:3306..."
+
+timeout 22 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' "${mysql_host}" 3306
+
+export PAPERLESS_DBHOST=${mysql_host}
+export PAPERLESS_DBPORT="3306"
+export PAPERLESS_DBNAME="test"
+export DB_USER="test"
+export PAPERLESS_DBPASS="example"
+export PAPERLESS_DBENGINE="mysql"  # or "mysql" for MariaDB
+export PAPERLESS_CONSUMPTION_DIR="/directory"
+
+export AUTHD_ACCOUNT=test
+export AUTHD_PASSWORD=testpassword
+
+DEBUG=true out/verify_pw_amd64
