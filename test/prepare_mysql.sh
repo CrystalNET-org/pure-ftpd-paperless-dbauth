039bdad31c5374d29e6fa38274084c4264053339
Lukas Wingerberg <bigluks@gmail.com>
2023-12-17 13:41:06 +0100

    test for auth

---
+++ test/prepare_mysql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+psql_host=$(env | grep PORT_5432_TCP_ADDR | cut -d "=" -f 2)
+echo "Waiting for postgresql to launch on ${psql_host}:5432..."
+
+timeout 22 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' "${psql_host}" 5432
+
+echo "postgresql launched, injecting sql"
+psql -h "${psql_host}" -d test -p 5432 -U test -f ./test/prep_psql.sql
---
+++ test/test_pw.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+psql_host=$(env | grep PORT_5432_TCP_ADDR | cut -d "=" -f 2)
+echo "Waiting for postgresql to launch on ${psql_host}:5432..."
+
+timeout 22 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' "${psql_host}" 5432
+
+export PAPERLESS_DBHOST=${psql_host}
+export PAPERLESS_DBPORT="5432"
+export PAPERLESS_DBNAME=${POSTGRES_DB}
+export DB_USER="test"
+export PAPERLESS_DBPASS="example"
+export PAPERLESS_DBENGINE="postgres"  # or "mysql" for MariaDB
+export PAPERLESS_CONSUMPTION_DIR="/directory"
+
+export AUTHD_ACCOUNT=test
+export AUTHD_PASSWORD=testpassword
+
+out/verify_pw_amd64
