1e66f5d483b5c17866bc168021b7bdcb82aee0f3
Lukas Wingerberg <bigluks@gmail.com>
2023-12-17 13:29:13 +0100

    replace sleep with more intelligent nc based waiting

--- test/prepare_postgresql.sh
+++ test/prepare_postgresql.sh
@@ -3,9 +3,7 @@
 psql_host=$(env | grep PORT_5432_TCP_ADDR | cut -d "=" -f 2)
 echo "Waiting for postgresql to launch on ${psql_host}:5432..."
 
-while ! nc -z "${psql_host}" 5432; do   
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
+timeout 22 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' "${psql_host}" 5432
 
 echo "postgresql launched, injecting sql"
 psql -h "${psql_host}" -d test -p 5432 -U test -f ./test/prep_psql.sql
