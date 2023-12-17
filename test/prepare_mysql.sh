4e59e1553957682d2961711760902c3268c984cd
Lukas Wingerberg <bigluks@gmail.com>
2023-12-17 13:45:20 +0100

    fixpassword hash

--- test/prepare_mysql.sh
+++ test/prepare_mysql.sh
@@ -6,4 +6,4 @@ echo "Waiting for postgresql to launch on ${psql_host}:5432..."
 timeout 22 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' "${psql_host}" 5432
 
 echo "postgresql launched, injecting sql"
-psql -h "${psql_host}" -d test -p 5432 -U test -f ./test/prep_psql.sql
+psql -h "${psql_host}" -d test -p 5432 -U test -f ./test/prep_sql.sql
