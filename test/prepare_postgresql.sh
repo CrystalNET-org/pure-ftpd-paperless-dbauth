93eabb33f0c1db66a1681e979a061687f7b96e7c
Lukas Wingerberg <bigluks@gmail.com>
2023-12-17 13:22:45 +0100

    add script

--- test/prepare_postgresql.sh
+++ test/prepare_postgresql.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
 
 sleep 30s
 psql_host=$(env | grep PORT_5432_TCP_ADDR | cut -d "=" -f 2)
 echo "connecting to ${psql_host}"
-psql -h "${psql_host}" -d test -p 5432 -U test -f ./sql/prep_psql.sql
+psql -h "${psql_host}" -d test -p 5432 -U test -f ./test/prep_psql.sql
